e0664b529d6a0ec1863dc86d70918cbdfb9350e7
nfransec <nfransec@gmail.com>
2024-10-11 22:47:33 +0530

    Fixed getdatafromtoken

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,16 +1,28 @@
 import { NextRequest } from "next/server";
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface DecodedToken extends JwtPayload {
+    userId: string;
+    email: string;
+}
 
 export const getDataFromToken = (request: NextRequest) => {
     try {
         const token = request.cookies.get("token")?.value || '';
-        const decodedToken: any = jwt.verify(token, process.env.NEXT_PUBLIC_TOKEN_SECRET!);
-        return decodedToken.id;
+
+        const tokenSecret = process.env.NEXT_PUBLIC_TOKEN_SECRET;
+        if(!tokenSecret) {
+            throw new Error("Token secret is not defined in the env variables")
+        }
+
+        const decodedToken = jwt.verify(token, tokenSecret) as DecodedToken;
+
+        return decodedToken.userId;
     } catch (error: unknown) {
         if (error instanceof Error) {
-            throw new Error(error.message);
+            throw new Error(`Token verification failed: ${error.message}`);
         } else {
-            throw new Error('An unknown error occurred');
+            throw new Error('An unknown error occured during token verification.')
         }
     }
-}
+};
