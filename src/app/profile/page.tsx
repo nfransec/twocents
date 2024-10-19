f1749b892267cc511cd7e5979c1454a9a22ecb91
nfransec <nfransec@gmail.com>
2024-10-19 20:30:27 +0530

    fixed lint error

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -158,7 +158,7 @@ export default function ProfilePage() {
       <div className="flex-1 overflow-auto">
         <header className="shadow-sm">
           <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-            <h1 className="text-2xl font-semibold text-white mt-4">{user?.fullName?.split(' ')[0]}'s dashboard</h1>
+            <h1 className="text-2xl font-semibold text-white mt-4">{user?.fullName?.split(' ')[0]}&#39;s dashboard</h1>
             <div className="flex items-center">
               <Bell className="h-5 w-5 text-gray-400 mr-4" />
               <div className="flex items-center">
