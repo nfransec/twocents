acf3ce98f1f5e5c6ab4e0c13bbe277588dc90e5a
nfransec <nfransec@gmail.com>
2024-10-27 12:18:06 +0530

    F1002548795

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { cn } from '@/lib/utils';
 import { ThemeProvider } from "@/components/theme-provider";
 import ConditionalBottomNav from "@/components/ConditionalBottomNav";
 import ConditionalHeader from "@/components/ConditionalHeader";
-import { useTokenExpiryCheck } from "@/hooks/useTokenExpiryCheck";
+
 
 const fontSans = Plus_Jakarta_Sans({ 
   subsets: ['latin'],
