688ad409045379458078a38abefc6f8553a6f048
nfransec <nfransec@gmail.com>
2025-02-01 22:45:27 +0530

    removed nextauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,1 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-
-export async function isAuthenticatedServer() {
-    const cookieStore = cookies()
-    return !!cookieStore.get('auth_token')
-}
-
-export async function isAuthenticatedClient() {
-    return !!localStorage.getItem('auth_token')
-}
-
-export async function logout() {
-    cookies().delete('auth_token')
-    localStorage.removeItem('auth_token')
-    redirect('/login')
-}
+// Remove this file entirely since we're not using NextAuth
