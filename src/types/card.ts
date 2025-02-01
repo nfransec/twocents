433ee455d071e4cc56d5c502c6430099dc1b0050
nfransec <nfransec@gmail.com>
2025-02-01 23:08:10 +0530

    Added type definitions for card

---
+++ src/types/card.ts
@@ -0,0 +1,28 @@
+export interface CardType {
+  _id: string;
+  cardName: string;
+  bankName: string;
+  cardLimit: number;
+  billingDate: string;
+  outstandingAmount: number;
+  cardNumber?: string;
+  paymentDate?: string;
+  paidAmount?: number;
+  isPaid: boolean;
+  paymentHistory?: {
+    amount: number;
+    date: string;
+    billingMonth: string;
+    outstandingAfterPayment: number;
+  }[];
+  lastPaymentAmount?: number;
+  lastPaymentDate?: string;
+  userId?: string;
+}
+
+export interface PaymentHistoryType {
+  amount: number;
+  date: string;
+  billingMonth: string;
+  outstandingAfterPayment: number;
+} 
